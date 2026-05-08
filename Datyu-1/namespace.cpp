b48a682201591367f30edaa096675fc8459bd1c6
rrpriyanshu0420 <priyanshu.23bcon1171@jecrcu.edu.in>
2026-05-08 11:40:57 +0530

    normal commit

--- Datyu-1/namespace.cpp
+++ Datyu-1/namespace.cpp
@@ -1,2 +1,20 @@
 #include<iostream>
 using namespace std;
+namespace virat{
+    void show()
+    {
+        cout<<"He is the part of RCB"<<endl;
+    }
+
+} 
+namespace rohit{
+    void show(){
+        cout<<"he is the part of MI"<<endl;
+    }
+}
+int main(){
+
+    virat:: show();
+    rohit:: show();
+    return 0;
+}
